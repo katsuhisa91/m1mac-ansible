86b1dea642220f3ab31abc9bdb5afece2506a1ad
katsuhisa91 <91katsuhisa@gmail.com>
2021-02-14 00:26:48 +0900

    add ansible galaxy

--- setting.zsh
+++ setting.zsh
@@ -5,6 +5,9 @@ ANSIBLE_PATH=~/workspace/settings/m1mac-ansible
 ## clone repo
 git clone https://github.com/katsuhisa91/m1mac-ansible.git ${ANSIBLE_PATH}
 
+## ansible galaxy
+ansible-galaxy collection install community.general
+
 ## ansible
 cd ${ANSIBLE_PATH}
 ansible-playbook -i inventory/localhost localhost.yml
